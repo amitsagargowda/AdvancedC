f34ad9c7db34c392666ab421feb54999d3da5b94
viveky1794 <viveky1794@gmail.com>
2021-08-27 14:32:00 +0530

    1. I have added one folder under Excercises name Students Where I did my one project. 2. I have create a folder of my name where i did custom shell programming. 3. Now it is for others So it can be evaluate and observe. 4. Any feedback is highly appricaited.

---
+++ Excercises/Students/vivek/myshell3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <error.h>
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+#define STDOUT 1
+#define STDIN 0
+
+void parshCommands(char *buff, char **argv){
+int sumlen=0;
+int len = strlen(buff);
+
+for(int i=0, j=0; i < len; ++i ){
+	if( buff[i] == ' ' || buff[i] == '\n' ){
+		buff[i] = '\0';
+	}
+}
+
+int i=0;
+for( i=0; sumlen < len ; ++i){
+argv[i] = ( buff + sumlen ); 
+sumlen += strlen( (buff+sumlen) );
+sumlen++;
+//printf("## %s\n", argv[i]);
+}
+argv[i] = NULL;
+//printf("## %s\n", argv[i]);
+}
+
+int main(void ){
+	
+	char tempbuff[512] = { "\n*****************************\n \
+		                \nWelcome to my cuatom shell\n \
+                                \n*****************************\n" };	       
+
+	write(STDOUT, tempbuff, sizeof(tempbuff));
+
+	/* @ Take commands form user.
+	 *
+	 */
+	while(1){
+	memset(tempbuff, '\0', sizeof(tempbuff));
+        write(STDOUT, "\nmyshell>> ", sizeof("\nmyshell>> ") );
+	read(STDIN, tempbuff, sizeof(tempbuff));
+	char *argv[50];
+        parshCommands(tempbuff, argv);
+	execvp(argv[0], argv);
+	
+	}//end-while
+	
+
+return 0;
+}
